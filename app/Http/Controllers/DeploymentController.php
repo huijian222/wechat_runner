714ec68d2a88f308eac2c522b4c1b292a1668894
19639804@qq.com <19639804@qq.com>
2016-08-30 14:42:26 +0800

    增加hook功能

---
+++ app/Http/Controllers/DeploymentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class DeploymentController extends Controller
+{
+    public function hook(){
+        $token = 'justatest';
+        $cmd = "  cd /var/www/test &&git pull &&echo 123";
+        $json = json_decode(file_get_contents('php://input') , true);
+        if(empty($json['token']) || $json['token'] !== $token){
+            exit('error request');
+        }
+        shell_exec($cmd);
+    }
+}
