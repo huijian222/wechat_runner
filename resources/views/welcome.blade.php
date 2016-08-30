6cc70ac25b58eff2cd9466aa8cbf350bf8b084fc
19639804@qq.com <19639804@qq.com>
2016-08-30 14:52:29 +0800

    测试hook是否正常

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TEST
                 </div>
 
                 <div class="links">
