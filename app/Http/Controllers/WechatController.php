3e4f5710e94d9d94274eb078ccf7a015043fba16
19639804@qq.com <19639804@qq.com>
2016-10-12 10:27:48 +0800

    fix for regrx

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -36,10 +36,13 @@ class WechatController extends Controller
                     }
                     if($message->Content == 'test'){
                         //return '你好 ' . $message->FromUserName . '我是你爸爸赖金榜';
+                        $flag = 1;
                         return '您好,绑定跑步账号请回复学号,纯数字PLZ';
                     }
-                    if(preg_match('/[0-9]+/' , $message->Content , $getNumber)){
-                        return $getNumber;
+                    if($flag == 1 && $message->Content != ''){
+                        //preg_match('/[0-9]+/' , $message->Content , $getNumber)
+                        $flag = 0;
+                        return $message->Content;
                     }
                     return '想要查看天气输入天气 , 查看跑步输入跑步,其他功能暂未实现,抱歉。';
                     break;
