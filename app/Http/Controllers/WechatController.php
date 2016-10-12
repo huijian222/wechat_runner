3e723d29613de05634db0118f4b449e9260ff396
19639804@qq.com <19639804@qq.com>
2016-10-12 12:05:48 +0800

    test for shujuku

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -39,7 +39,7 @@ class WechatController extends Controller
                     }
                     if(preg_match_all('/[0-9]+/' , $message->Content , $getNumber)!= 0){
                         preg_match('/[0-9]+/' , $message->Content , $getNumber);
-                        if($wechatuser->where('username' , $getNumber[0])!=''){
+                        if($wechatuser->where('username' ,'=', $getNumber[0])->get()!=''){
                             return 'test';
                         }
                         return $getNumber[0];
