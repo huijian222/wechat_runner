816338cfaa2e8cb0060c0b0ae5def2e4b20779ad
19639804@qq.com <19639804@qq.com>
2016-10-12 11:05:20 +0800

    test for while

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -35,11 +35,15 @@ class WechatController extends Controller
                         $pe = $weneed[0].'同学,你好! 您的总次数为'.$weneed[5];
                         return $pe;
                     }
-                    while($message->Content == 'test'){
+                    while($message->Content == '绑定'){
                         //return '你好 ' . $message->FromUserName . '我是你爸爸赖金榜';
                         //return '您好,绑定跑步账号请回复学号,纯数字PLZ';
-                        if($message->Content == 'test2'){
-                            return 123321;
+                        if(preg_match_all('/[0-9]+/' , $message->Content , $getNumber)!= 0){
+                            preg_match('/[0-9]+/' , $message->Content , $getNumber);
+                            return $getNumber[0];
+                        }
+                        else{
+                            break;
                         }
                     }
                     if(preg_match_all('/[0-9]+/' , $message->Content , $getNumber)!= 0){
