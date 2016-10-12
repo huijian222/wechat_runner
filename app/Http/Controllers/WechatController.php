f08506348c0dfa6f9a1365d29f7cf76b9f55280c
19639804@qq.com <19639804@qq.com>
2016-10-12 10:57:41 +0800

    test regex for text

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -40,7 +40,7 @@ class WechatController extends Controller
                         //return '您好,绑定跑步账号请回复学号,纯数字PLZ';
                     }
                     if(preg_match_all('/[0-9]+/' , $message->Content , $getNumber)!= 0){
-                        preg_match('/[0-9]+/' , $message->Content , $getNumber)
+                        preg_match('/[0-9]+/' , $message->Content , $getNumber);
                         return $getNumber[0];
                     }
                     return '想要查看天气输入天气 , 查看跑步输入跑步,其他功能暂未实现,抱歉。';
